29d471943f3c616509e6757de5d93a08eda02cd5
moolchandra rathour <59133080+rathourmool@users.noreply.github.com>
2021-10-23 12:26:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,64 +1,72 @@
-const allSideMenu = document.querySelectorAll('#sidebar .side-menu.top li a');
+var selectedRow = null;
+function onFormSubmit(e){
+    event.preventDefault();
+    var formData = readFormData();
+    if(selectedRow === null){
+        insertNewRecord(formData);
+    }
+    else{
+        updateRecord(formData);
+    }
+    resetForm();
+}
 
-allSideMenu.forEach(item=> {
-	const li = item.parentElement;
+//Retrieve the data
+function readFormData(){
+    var formData = {};
+    formData["projectcode"] = document.getElementById("projectcode").value;
+    formData["projectdate"] = document.getElementById("projectdate").value;
+    formData["status"] = document.getElementById("status").value;
+    formData["perPrice"] = document.getElementById("perPrice").value;
+    return formData;
+}
 
-	item.addEventListener('click', function () {
-		allSideMenu.forEach(i=> {
-			i.parentElement.classList.remove('active');
-		})
-		li.classList.add('active');
-	})
-});
+//Insert the data
+function insertNewRecord(data){
+    var table = document.getElementById("storeList").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    var cell1 = newRow.insertCell(0);
+        cell1.innerHTML = data.projectcode;
+    var cell2 = newRow.insertCell(1);
+        cell2.innerHTML = data.projectdate;
+    var cell3 = newRow.insertCell(2);
+        cell3.innerHTML = data.status;
+    var cell4 = newRow.insertCell(3);
+        cell4.innerHTML = data.perPrice;
+    var cell5 = newRow.insertCell(4);
+        cell5.innerHTML = `<button onClick='onEdit(this)'>Edit</button> <button onClick='onDelete(this)'>Delete</button>`
+}
 
+//Edit the data
+function onEdit(td){
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById('projectcode').value = selectedRow.cells[0].innerHTML;
+    document.getElementById('projectdate').value = selectedRow.cells[1].innerHTML;
+    document.getElementById('status').value = selectedRow.cells[2].innerHTML;
+    document.getElementById('perPrice').value = selectedRow.cells[3].innerHTML;
+}
 
+function updateRecord(formData){
+    selectedRow.cells[0].innerHTML = formData.projectcode;
+    selectedRow.cells[1].innerHTML = formData.projectdate;
+    selectedRow.cells[2].innerHTML = formData.status;
+    selectedRow.cells[3].innerHTML = formData.perPrice;
+}
 
-
-// TOGGLE SIDEBAR
-const menuBar = document.querySelector('#content nav .bx.bx-menu');
-const sidebar = document.getElementById('sidebar');
-
-menuBar.addEventListener('click', function () {
-	sidebar.classList.toggle('hide');
-})
-
-
-
-
-
-
-
-const searchButton = document.querySelector('#content nav form .form-input button');
-const searchButtonIcon = document.querySelector('#content nav form .form-input button .bx');
-const searchForm = document.querySelector('#content nav form');
-
-searchButton.addEventListener('click', function (e) {
-	if(window.innerWidth < 576) {
-		e.preventDefault();
-		searchForm.classList.toggle('show');
-		if(searchForm.classList.contains('show')) {
-			searchButtonIcon.classList.replace('bx-search', 'bx-x');
-		} else {
-			searchButtonIcon.classList.replace('bx-x', 'bx-search');
-		}
-	}
-})
-
-
-
-
-
-if(window.innerWidth < 768) {
-	sidebar.classList.add('hide');
-} else if(window.innerWidth > 576) {
-	searchButtonIcon.classList.replace('bx-x', 'bx-search');
-	searchForm.classList.remove('show');
+//Delete the data
+function onDelete(td){
+    if(confirm('Do you want to delete this record?')){
+        row = td.parentElement.parentElement;
+        document.getElementById('storeList').deleteRow(row.rowIndex);
+    }
+    resetForm();
 }
 
 
-window.addEventListener('resize', function () {
-	if(this.innerWidth > 576) {
-		searchButtonIcon.classList.replace('bx-x', 'bx-search');
-		searchForm.classList.remove('show');
-	}
-})
+//Reset the data
+function resetForm(){
+    document.getElementById('projectCode').value = '';
+    document.getElementById('projectdate').value = '';
+    document.getElementById('status').value = '';
+    document.getElementById('perPrice').value = '';
+}
